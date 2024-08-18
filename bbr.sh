d3a8b1976fa52c53b8d20fb9ebdbaa6bb7351ee1
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-18 07:15:39 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -1,7 +1,6 @@
 bash <(curl -LS https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/google-bbr.sh)
 
 # Define the settings
-sudo modprobe nf_conntrack
 Sysctl_file="/etc/sysctl.conf"
 sudo sed -i '/net\.core\.default_qdisc/d' $Sysctl_file
 sudo sed -i '/net\.ipv4\.tcp_congestion_control/d' $Sysctl_file
@@ -22,25 +21,25 @@ net.core.wmem_max = 67108864
 net.core.rmem_default = 65536
 net.core.wmem_default = 65536
 net.core.netdev_max_backlog = 250000
-net.core.somaxconn = 8192
+net.core.somaxconn = 32768
 net.ipv4.tcp_syncookies = 1
 net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_fin_timeout = 30
 net.ipv4.tcp_keepalive_time = 600
-net.ipv4.tcp_max_syn_backlog = 8192
-
-net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_max_syn_backlog = 16384
+fs.inotify.max_user_instances = 8192
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.ipv4.tcp_max_orphans = 32768
 net.ipv4.tcp_mem = 25600 51200 102400
 net.ipv4.tcp_rmem = 4096 65536 67108864
 net.ipv4.tcp_wmem = 4096 65536 67108864
 net.ipv4.tcp_mtu_probing = 1
-
+net.ipv4.ip_local_port_range = 1024 65000
 # Additional settings
 net.ipv4.ip_forward = 1
-net.netfilter.nf_conntrack_max = 2097152
-net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
-net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 60
-net.netfilter.nf_conntrack_tcp_timeout_time_wait = 60
 net.ipv4.conf.all.route_localnet = 1
 EOF
 sysctl -p
