8dcc45618b45cc67db701e3bcc5a65283f0d7627
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-08-28 12:18:14 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -35,42 +35,37 @@ sudo sed -i '/net\.core\.default_qdisc/d' $Sysctl_file
 sudo sed -i '/net\.ipv4\.tcp_congestion_control/d' $Sysctl_file
 sudo modprobe nf_conntrack
 cat >> $Sysctl_file <<EOF
-# Common settings
-net.ipv4.tcp_congestion_control = bbr
-net.core.default_qdisc = fq
+fs.file-max = 1000000
+fs.inotify.max_user_instances = 8192
 
-# Common settings
-fs.file-max = 200000
-net.core.rmem_max = 67108864
-net.core.wmem_max = 67108864
-net.core.rmem_default = 65536
-net.core.wmem_default = 65536
-net.core.netdev_max_backlog = 250000
-net.core.somaxconn = 4096
 net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_fin_timeout = 30
 net.ipv4.tcp_tw_reuse = 1
-net.ipv4.tcp_fin_timeout = 10
-net.ipv4.tcp_keepalive_time = 600
-net.ipv4.ip_local_port_range = 10000 65000
-net.ipv4.tcp_max_syn_backlog = 8192
+net.ipv4.ip_local_port_range = 1024 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 6000
+net.ipv4.route.gc_timeout = 100
 
-net.ipv4.tcp_max_tw_buckets = 5000
-net.ipv4.tcp_fastopen = 3
-net.ipv4.tcp_mem = 25600 51200 102400
-net.ipv4.tcp_rmem = 4096 65536 67108864
-net.ipv4.tcp_wmem = 4096 65536 67108864
-net.ipv4.tcp_mtu_probing = 1
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 32768
+net.core.netdev_max_backlog = 32768
+net.ipv4.tcp_timestamps = 0
+net.ipv4.tcp_max_orphans = 32768
 
-# Additional settings
+# forward ipv4
 net.ipv4.ip_forward = 1
-net.netfilter.nf_conntrack_max = 2097152
-net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
-net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 60
-net.netfilter.nf_conntrack_tcp_timeout_time_wait = 60
-
-net.ipv4.conf.all.route_localnet = 1
 EOF
+    cat >> /etc/security/limits.conf <<-EOF
+*               soft    nofile          1000000
+*               hard    nofile          1000000
+EOF
+
+echo "ulimit -SHn 1000000" >> /etc/profile
+source /etc/profile
+
 sysctl -p
+
 echo "System settings have been applied."
 SSH_PATH="/etc/ssh/sshd_config"
 PROF_PATH="/etc/profile"
