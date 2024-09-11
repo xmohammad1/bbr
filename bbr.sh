c8d4a1ce83a25c3df15d17b47a43c0ee57e99fb3
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2024-09-11 13:38:35 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -6,28 +6,22 @@ echo "Hosts Fixed."
 fi
 bash <(curl -LS https://raw.githubusercontent.com/hiddify/Hiddify-Manager/main/common/google-bbr.sh)
 #Define the module name
-# MODULE_NAME="nf_conntrack"
+MODULE_NAME="nf_conntrack"
 
-# # Define the path to the configuration file
-# CONF_FILE="/etc/modules-load.d/${MODULE_NAME}.conf"
+# Define the path to the configuration file
+CONF_FILE="/etc/modules-load.d/${MODULE_NAME}.conf"
 
-# # Check if the configuration file already exists
-# if [ -f "$CONF_FILE" ]; then
-#     echo "Configuration file $CONF_FILE already exists."
-# else
-#     # Create a new configuration file
-#     echo "$MODULE_NAME" | sudo tee "$CONF_FILE" > /dev/null
-# fi
+# Check if the configuration file already exists
+if [ -f "$CONF_FILE" ]; then
+    echo "Configuration file $CONF_FILE already exists."
+else
+    # Create a new configuration file
+    echo "$MODULE_NAME" | sudo tee "$CONF_FILE" > /dev/null
+fi
 
-# # Load the module immediately (without reboot)
-# sudo modprobe "$MODULE_NAME"
+# Load the module immediately (without reboot)
+sudo modprobe "$MODULE_NAME"
 
-# # Verify if the module is loaded
-# if lsmod | grep -q "$MODULE_NAME"; then
-#     echo "Module $MODULE_NAME is successfully loaded."
-# else
-#     echo "Failed to load the module $MODULE_NAME."
-# fi
 
 # Define the settings
 Sysctl_file="/etc/sysctl.conf"
@@ -39,26 +33,35 @@ net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 net.ipv4.tcp_ecn=2
 
-fs.file-max = 1000000
-fs.inotify.max_user_instances = 8192
-
+# Common settings
+fs.file-max = 200000
+net.core.rmem_max = 67108864
+net.core.wmem_max = 67108864
+net.core.rmem_default = 65536
+net.core.wmem_default = 65536
+net.core.netdev_max_backlog = 250000
+net.core.somaxconn = 4096
 net.ipv4.tcp_syncookies = 1
-net.ipv4.tcp_fin_timeout = 30
 net.ipv4.tcp_tw_reuse = 1
-net.ipv4.ip_local_port_range = 1024 65000
-net.ipv4.tcp_max_syn_backlog = 16384
-net.ipv4.tcp_max_tw_buckets = 6000
-net.ipv4.route.gc_timeout = 100
+net.ipv4.tcp_fin_timeout = 10
+net.ipv4.tcp_keepalive_time = 600
+net.ipv4.ip_local_port_range = 10000 65000
+net.ipv4.tcp_max_syn_backlog = 8192
 
-net.ipv4.tcp_syn_retries = 1
-net.ipv4.tcp_synack_retries = 1
-net.core.somaxconn = 32768
-net.core.netdev_max_backlog = 32768
-net.ipv4.tcp_timestamps = 0
-net.ipv4.tcp_max_orphans = 32768
+net.ipv4.tcp_max_tw_buckets = 5000
+net.ipv4.tcp_fastopen = 3
+net.ipv4.tcp_mem = 25600 51200 102400
+net.ipv4.tcp_rmem = 4096 65536 67108864
+net.ipv4.tcp_wmem = 4096 65536 67108864
+net.ipv4.tcp_mtu_probing = 1
 
-# forward ipv4
+# Additional settings
 net.ipv4.ip_forward = 1
+net.netfilter.nf_conntrack_max = 2097152
+net.netfilter.nf_conntrack_tcp_timeout_close_wait = 60
+net.netfilter.nf_conntrack_tcp_timeout_fin_wait = 60
+net.netfilter.nf_conntrack_tcp_timeout_time_wait = 60
+net.ipv4.conf.all.route_localnet = 1
 EOF
     cat >> /etc/security/limits.conf <<-EOF
 *               soft    nofile          1000000
