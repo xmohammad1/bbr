50e195d7ff16db02ac1d388850aa19ed1b188a0f
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-04-23 19:16:47 +0330

    Update bbr.sh

--- bbr.sh
+++ bbr.sh
@@ -41,25 +41,7 @@ kernel.threads-max = 100000
 kernel.pid_max = 100000
 vm.max_map_count = 262144
 EOF
-if [[ $(lsb_release -rs) != "24.04" ]]; then
-    # # Define the settings
-    # sudo sed -i '/net\.core\.default_qdisc/d' $Sysctl_file
-    # sudo sed -i '/net\.ipv4\.tcp_congestion_control/d' $Sysctl_file
-    # #Define the module name
-    # MODULE_NAME="nf_conntrack"
 
-    # # Define the path to the configuration file
-    # CONF_FILE="/etc/modules-load.d/${MODULE_NAME}.conf"
-
-    # Check if the configuration file already exists
-    # if [ -f "$CONF_FILE" ]; then
-    #     echo "Configuration file $CONF_FILE already exists."
-    # else
-    #     # Create a new configuration file
-    #     echo "$MODULE_NAME" | sudo tee "$CONF_FILE" > /dev/null
-    # fi
-    # sudo modprobe "$MODULE_NAME"
-fi
 cat >> /etc/security/limits.conf <<-EOF
 *               soft    nofile          1000000
 *               hard    nofile          1000000
