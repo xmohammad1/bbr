6185211c6dedd786c236a560b59e254ec321e922
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-06-06 05:04:09 +0330

    Update set-unbound-dns.sh

--- set-unbound-dns.sh
+++ set-unbound-dns.sh
@@ -342,6 +342,7 @@ server:
     interface: 127.0.0.1
     interface: ::1
     port: 53
+    module-config: "iterator"
     serve-expired: yes
     serve-expired-ttl: 86400
     serve-expired-client-timeout: 0
