59e435de7ce4e8c8f76ccbd8baa47c70a38faf09
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-06-06 16:49:12 +0330

    Update set-unbound-dns.sh

--- set-unbound-dns.sh
+++ set-unbound-dns.sh
@@ -327,8 +327,8 @@ server:
     so-reuseport: yes
     cache-max-ttl: 86400
     cache-min-ttl: 60
-    msg-cache-size: 512m
-    rrset-cache-size: 512m
+    msg-cache-size: 256m
+    rrset-cache-size: 256m
     prefetch: yes
     prefetch-key: yes
     do-ip4: yes
