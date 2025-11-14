3b5b16d0b51e354c84ab29ba262eec3eee109820
xmohammad1 <117955130+xmohammad1@users.noreply.github.com>
2025-11-14 05:15:31 +0330

    Update set-unbound-dns.sh

--- set-unbound-dns.sh
+++ set-unbound-dns.sh
@@ -381,7 +381,6 @@ $( [ "$enable_ipv6" = "yes" ] && echo "    private-address: fe80::/10" )
 forward-zone:
     name: "."
     forward-first: no
-    forward-tls-upstream: no
     forward-addr: ${primary_dns}
     forward-addr: ${secondary_dns}
 EOF
